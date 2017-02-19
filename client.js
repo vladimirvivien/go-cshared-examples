cdeda64ec7e85a26ae567caef509407d139d4c62
Vladimir Vivien <vladimir.vivien@gmail.com>
2017-02-19 09:25:06 -0500

    Fixed client.js to work properly

--- client.js
+++ client.js
@@ -3,28 +3,39 @@ var ffi = require("ffi");
 var Struct = require("ref-struct")
 var ArrayType = require("ref-array")
 
+var longlong = ref.types.longlong;
+var LongArray = ArrayType(longlong);
+
 var GoSlice = Struct({
-  data: "void *",
+  data: LongArray,
   len:  "longlong",
   cap: "longlong"
 });
 
+var GoString = Struct({
+  p: "string",
+  n: "longlong"
+});
+
 var awesome = ffi.Library("awesomelib/awesome.so", {
-  Add: ["long", ["long", "long"]],
+  Add: ["longlong", ["longlong", "longlong"]],
   Cosine: ["double", ["double"]], 
-  Sort: ["void", [GoSlice]]
+  Sort: ["void", [GoSlice]],
+  Log: ["longlong", [GoString]]
 });
 
 console.log("awesome.Add(12, 99) = ", awesome.Add(12, 99));
 console.log("awesome.Cosine(1) = ", awesome.Cosine(1));
 
-var longArr = ref.types.longlong
-var LongArrType = ArrayType(longArr)
-var nums = new LongArrType([53,66,2,12,5])
+nums = LongArray([12,54,0,423,9]);
+var slice = new GoSlice();
+slice["data"] = nums;
+slice["len"] = 5;
+slice["cap"] = 5;
+awesome.Sort(slice);
+console.log(nums.toArray());
 
-var slice = new GoSlice
-slice["data"] = 
-slice["len"] = nums.length
-slice["cap"] = nums.length
-awesome.Sort(slice)
-console.log(nums.toArray())
+str = new GoString();
+str["p"] = "Hello Node!";
+str["n"] = 11;
+awesome.Log(str);
