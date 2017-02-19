c7e8aaff686c7549fdf583057ba0fc2669208720
Vladimir Vivien <vladimir.vivien@gmail.com>
2017-02-19 07:37:58 -0500

    Java example works

--- Client.java
+++ Client.java
@@ -1,13 +1,70 @@
-import com.sun.jna.Library;
-import com.sun.jna.Native;
+import com.sun.jna.*;
+import java.util.*;
+import java.lang.Long;
 
-public class Cos {
-    public interface Awesome extends Library {
+public class Client {
+   public interface Awesome extends Library {
+        // GoSlice class maps to:
+        // C typedef struct { void *data; GoInt len; GoInt cap; }
+        public class GoSlice extends Structure {
+            public static class ByValue extends GoSlice implements Structure.ByValue {}
+            public Pointer data;
+            public long len;
+            public long cap;
+            protected List getFieldOrder(){
+                return Arrays.asList(new String[]{"data","len","cap"});
+            }
+        }
+
+        // GoString class maps to:
+        // C typedef struct { const char *p; GoInt n; }
+        public class GoString extends Structure {
+            public static class ByValue extends GoString implements Structure.ByValue {}
+            public String p;
+            public long n;
+            protected List getFieldOrder(){
+                return Arrays.asList(new String[]{"p","n"});
+            }
+
+        }
+
+        // Foreign functions
+        public long Add(long a, long b);
         public double Cosine(double val);
+        public void Sort(GoSlice.ByValue vals);
+        public long Log(GoString.ByValue str);
     }
  
    static public void main(String argv[]) {
-        Awesome awesome = (Awesome) Native.loadLibrary("awesomelib/awesome.so", Awesome.class);
-        System.out.print(awesome.Cosine(1.0));
+        Awesome awesome = (Awesome) Native.loadLibrary(
+            "awesomelib/awesome.so", Awesome.class);
+
+        System.out.printf("awesome.Add(12, 99) = %s\n", awesome.Add(12, 99));
+        System.out.printf("awesome.Cosine(1.0) = %s\n", awesome.Cosine(1.0));
+        
+        // Call Sort
+        // First, prepare data array 
+        long[] nums = new long[]{53,11,5,2,88};
+        Memory arr = new Memory(nums.length * Native.getNativeSize(Long.TYPE));
+        arr.write(0, nums, 0, nums.length); 
+        // fill in the GoSlice class for type mapping
+        Awesome.GoSlice.ByValue slice = new Awesome.GoSlice.ByValue();
+        slice.data = arr;
+        slice.len = nums.length;
+        slice.cap = nums.length;
+        awesome.Sort(slice);
+        System.out.print("awesome.Sort(53,11,5,2,88) = [");
+        long[] sorted = slice.data.getLongArray(0,nums.length);
+        for(int i = 0; i < sorted.length; i++){
+            System.out.print(sorted[i] + " ");
+        }
+        System.out.println("]");
+
+        // Call Log
+        Awesome.GoString.ByValue str = new Awesome.GoString.ByValue();
+        str.p = "Hello Java!";
+        str.n = str.p.length();
+        System.out.printf("msgid %d\n", awesome.Log(str));
+
     }
 }
--- client.js
+++ client.js
@@ -1,7 +1,30 @@
-var ffi = require('ffi');
+var ref = require("ref");
+var ffi = require("ffi");
+var Struct = require("ref-struct")
+var ArrayType = require("ref-array")
 
-var awesome = ffi.Library('awesomelib/awesome.so', {
-  'Cosine': ['double', ['double']]
+var GoSlice = Struct({
+  data: "void *",
+  len:  "longlong",
+  cap: "longlong"
 });
 
-console.log(awesome.Cosine(1));
+var awesome = ffi.Library("awesomelib/awesome.so", {
+  Add: ["long", ["long", "long"]],
+  Cosine: ["double", ["double"]], 
+  Sort: ["void", [GoSlice]]
+});
+
+console.log("awesome.Add(12, 99) = ", awesome.Add(12, 99));
+console.log("awesome.Cosine(1) = ", awesome.Cosine(1));
+
+var longArr = ref.types.longlong
+var LongArrType = ArrayType(longArr)
+var nums = new LongArrType([53,66,2,12,5])
+
+var slice = new GoSlice
+slice["data"] = 
+slice["len"] = nums.length
+slice["cap"] = nums.length
+awesome.Sort(slice)
+console.log(nums.toArray())
