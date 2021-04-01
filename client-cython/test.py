724924cfe73e3e1a1f587606796dc15d22e900b1
synodriver <624805065@qq.com>
2021-04-01 17:34:27 +0800

    add cython example

---
+++ client-cython/test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import awesome
+
+print(f"awesome.Add(12,99) = {awesome.add(12, 99)}")
+
+print(f"awesome.Cosine(1) = {awesome.cosine(1)}")
+
+print(f"awesome.sort([74,4,122,9,12])  = {awesome.sort([74, 4, 122, 9, 12])}")
+
+i = awesome.log("I am cython!")
+
+print(f"log in {i}")
